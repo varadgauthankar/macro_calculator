27cc16625244de7ac233903bb070d7ee6b8cbf96
Varad Gauthankar <gvarad1601@gmail.com>
2022-07-05 12:45:11 +0530

    minor improvements

--- lib/pages/results_page.dart
+++ lib/pages/results_page.dart
@@ -46,61 +46,57 @@ class ResultPage extends StatelessWidget {
           children: [
             Screenshot(
               controller: screenshotController,
-              child: Container(
-                color:
-                    isThemeDark(context) ? MyColors.darkGrey : MyColors.white,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    ResultTile(
-                      title: "Total Calories",
-                      value: "${totalCalories.toStringAsFixed(0)}",
-                      units: "KCALS",
-                    ),
-                    ResultTile(
-                      title: "Carbs",
-                      value: "${carbs.toStringAsFixed(0)}",
-                      units: "GRAMS",
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: ResultTile(
-                            title: "Protein",
-                            value: "${protein.toStringAsFixed(0)}",
-                            units: "GRAMS",
-                          ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  ResultTile(
+                    title: "Total Calories",
+                    value: "${totalCalories.toStringAsFixed(0)}",
+                    units: "KCALS",
+                  ),
+                  ResultTile(
+                    title: "Carbs",
+                    value: "${carbs.toStringAsFixed(0)}",
+                    units: "GRAMS",
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: ResultTile(
+                          title: "Protein",
+                          value: "${protein.toStringAsFixed(0)}",
+                          units: "GRAMS",
                         ),
-                        Expanded(
-                          child: ResultTile(
-                            title: "Fats",
-                            value: "${fats.toStringAsFixed(0)}",
-                            units: "GRAMS",
-                          ),
+                      ),
+                      Expanded(
+                        child: ResultTile(
+                          title: "Fats",
+                          value: "${fats.toStringAsFixed(0)}",
+                          units: "GRAMS",
                         ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: ResultTile(
-                            title: "BMI",
-                            value: "${bmi.toStringAsFixed(1)}",
-                            units: bmiScale,
-                          ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: ResultTile(
+                          title: "BMI",
+                          value: "${bmi.toStringAsFixed(1)}",
+                          units: bmiScale,
                         ),
-                        Expanded(
-                          child: ResultTile(
-                            title: "TDEE",
-                            value: "${tdee.toStringAsFixed(0)}",
-                            units: "KCALS",
-                          ),
+                      ),
+                      Expanded(
+                        child: ResultTile(
+                          title: "TDEE",
+                          value: "${tdee.toStringAsFixed(0)}",
+                          units: "KCALS",
                         ),
-                      ],
-                    ),
-                  ],
-                ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
           ],
@@ -120,8 +116,10 @@ class ResultPage extends StatelessWidget {
     String tempPath = (await getTemporaryDirectory()).path;
     File file = File('$tempPath/img.png');
     await file.writeAsBytes(unit8List);
-    await Share.shareFiles([file.path],
-        text: 'Calculated from Macro Calculator.\n' +
-            'download now: https://play.google.com/store/apps/details?id=com.varadgauthankar.macro_calculator');
+    await Share.shareFiles(
+      [file.path],
+      text: 'Calculated from Macro Calculator.\n' +
+          'download now: https://play.google.com/store/apps/details?id=com.varadgauthankar.macro_calculator',
+    );
   }
 }
